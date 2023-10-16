8fd70058e02f9eca2fcbc9f6abe6cb48ac470f2c
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-10-16 16:27:43 -0300

    Add styling to flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  FLASH_CLASSES = {
+    notice: "alert alert-info",
+    success: "alert alert-success",
+    error: "alert alert-danger",
+    alert: "alert alert-danger"
+  }.freeze
+
+  def flash_class(level)
+    FLASH_CLASSES[level.to_sym]
+  end
 end
