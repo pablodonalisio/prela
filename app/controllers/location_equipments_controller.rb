bd21c2d4e660437ed1de653d7e4929149661169d
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-10-21 20:21:49 -0300

    Create edit and destroy location_equipment flow

--- app/controllers/location_equipments_controller.rb
+++ app/controllers/location_equipments_controller.rb
@@ -1,8 +1,13 @@
 class LocationEquipmentsController < ApplicationController
+  before_action :equipments, only: %i[new edit update destroy]
+  before_action :client
+  before_action :location_equipment, only: %i[edit update destroy]
+
   def new
     @location_equipment = LocationEquipment.new
-    @equipments = Equipment.all
-    client
+  end
+
+  def edit
   end
 
   def create
@@ -18,6 +23,26 @@ class LocationEquipmentsController < ApplicationController
     end
   end
 
+  def update
+    if @location_equipment.update(location_equipment_params)
+      respond_to do |format|
+        format.html { redirect_to client_path(client), notice: "Se ha actualizado el equipo de la sede." }
+        format.turbo_stream { flash.now[:notice] = "Se ha actualizado el equipo de la sede." }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @location_equipment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to client_path(client), notice: "El equipo de la sede ha sido eliminado." }
+      format.turbo_stream { flash.now[:notice] = "El equipo de la sede ha sido eliminado." }
+    end
+  end
+
   private
 
   def location_equipment_params
@@ -26,5 +51,13 @@ class LocationEquipmentsController < ApplicationController
 
   def client
     @client ||= Client.find(params[:client_id])
+  end
+
+  def equipments
+    @equipments ||= Equipment.all
+  end
+
+  def location_equipment
+    @location_equipment ||= LocationEquipment.find(params[:id])
   end
 end
