7589f2a89d996f703966b50f05765192d4c97d31
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-10-30 15:25:03 -0300

    fix(clients): do not include location_equipments in controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -53,7 +53,7 @@ class ClientsController < ApplicationController
   private
 
   def set_client
-    @client = Client.includes(locations: :location_equipments).find(params[:id])
+    @client = Client.includes(:locations).find(params[:id])
   end
 
   def client_params
