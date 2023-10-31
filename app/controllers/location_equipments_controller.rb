8f74fd51345d4ab7da1012ce9413e4eb2e789f80
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-10-31 18:42:09 -0300

    refactor(location_equipments_controller): remove unnecesary methods

--- app/controllers/location_equipments_controller.rb
+++ app/controllers/location_equipments_controller.rb
@@ -1,5 +1,4 @@
 class LocationEquipmentsController < ApplicationController
-  before_action :equipments, only: %i[new edit update destroy]
   before_action :location_equipment, only: %i[edit update destroy]
 
   def new
@@ -19,7 +18,7 @@ class LocationEquipmentsController < ApplicationController
 
     if @location_equipment.save
       respond_to do |format|
-        format.html { redirect_to client_path(client), notice: "Se ha agregado un nuevo equipo a la sede." }
+        format.html { redirect_to location_equipments_path, notice: "Se ha agregado un nuevo equipo a la sede." }
         format.turbo_stream { flash.now[:notice] = "Se ha agregado un nuevo equipo a la sede." }
       end
     else
@@ -31,7 +30,7 @@ class LocationEquipmentsController < ApplicationController
   def update
     if @location_equipment.update(location_equipment_params)
       respond_to do |format|
-        format.html { redirect_to client_path(client), notice: "Se ha actualizado el equipo de la sede." }
+        format.html { redirect_to location_equipments_path, notice: "Se ha actualizado el equipo de la sede." }
         format.turbo_stream { flash.now[:notice] = "Se ha actualizado el equipo de la sede." }
       end
     else
@@ -43,7 +42,7 @@ class LocationEquipmentsController < ApplicationController
     @location_equipment.destroy
 
     respond_to do |format|
-      format.html { redirect_to client_path(client), notice: "El equipo de la sede ha sido eliminado." }
+      format.html { redirect_to location_equipments_path, notice: "El equipo de la sede ha sido eliminado." }
       format.turbo_stream { flash.now[:notice] = "El equipo de la sede ha sido eliminado." }
     end
   end
@@ -52,14 +51,6 @@ class LocationEquipmentsController < ApplicationController
 
   def location_equipment_params
     params.require(:location_equipment).permit(:zone, :floor, :location_id, :equipment_id)
-  end
-
-  def client
-    @client ||= Client.find(params[:client_id])
-  end
-
-  def equipments
-    @equipments ||= Equipment.all
   end
 
   def location_equipment
