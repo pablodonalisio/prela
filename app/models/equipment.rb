83d4655a86aee656df2d0e4f03c4cb3d6ab74f49
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-11-01 16:57:13 -0300

    feat(location_equipment): link to equipment

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,8 @@
 class Equipment < ApplicationRecord
   has_many :location_equipments, dependent: :destroy
   validates :kind, :brand, :model, presence: true
+
+  def full_name
+    "#{brand} - #{model}"
+  end
 end
