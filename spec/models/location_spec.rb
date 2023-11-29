bf21544ee5817635e1de5bc4b2d09e2f4d134ad0
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-11-29 10:37:36 -0300

    feat(Location): add tests for model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,11 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Location, type: :model do
-  it "is valid with valid attributes" do
-    expect(build(:client)).to be_valid
+  context "validations" do
+    let(:location) { build(:location) }
+
+    it "is valid with valid attributes" do
+      expect(location).to be_valid
+    end
+
+    it "is not valid without a name" do
+      location.name = nil
+      expect(location).not_to be_valid
+    end
   end
 
-  it "is not valid without a name" do
-    expect(build(:client, name: "")).not_to be_valid
+  context "associations" do
+    let(:location) { create(:location) }
+
+    it "belongs to a client" do
+      expect(location.client).to be_a(Client)
+    end
+
+    it "has many location equipments" do
+      create_list(:location_equipment, 3, location: location)
+      expect(location.location_equipments.size).to eq(3)
+    end
   end
 end
