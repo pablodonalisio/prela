6229fcc195bd1803f90b8e01f7d580a2e03b4e07
Pablo Donalisio <pablo.donalisio@gmail.com>
2023-11-30 17:22:20 -0300

    fix(Devise): delete routes for registrations and passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations, :passwords]
+  devise_scope :user do
+    get "users/edit" => "devise/registrations#edit", :as => "edit_user_registration"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
