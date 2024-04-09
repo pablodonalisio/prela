a4db85e007a017f7cac9f90cc5147db7f52a4501
Pablo Donalisio <pablo.donalisio@gmail.com>
2024-04-09 19:17:46 -0300

    Add observations to reports pdf

--- app/services/reports/pdf_generator.rb
+++ app/services/reports/pdf_generator.rb
@@ -11,6 +11,8 @@ class Reports::PdfGenerator < PdfGenerator
     add_equipment_stats_to_pdf
     @pdf.move_down 10
     add_room_stats_to_pdf
+    @pdf.move_down 10
+    add_observations_to_pdf
     super
   end
 
@@ -34,6 +36,16 @@ class Reports::PdfGenerator < PdfGenerator
 
   def add_room_stats_to_pdf
     @pdf = Reports::RoomStats.new(report, @pdf).render
+  end
+
+  def add_observations_to_pdf
+    @pdf.table([
+      [{content: "OBSERVACIONES", colspan: 3, background_color: PRIMARY_COLOR, font_style: :bold}],
+      [{content: report.observations, colspan: 3, height: 100, background_color: "f3f3f3"}]
+    ], width: @pdf.bounds.width) do
+      cells.borders = []
+      cells.width = @pdf.bounds.width / 3
+    end
   end
 
   def report
