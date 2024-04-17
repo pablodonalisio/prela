bbe94efaa32af04c7e9a2a7c9c3ea4f19f0b5f3b
Pablo Donalisio <pablo.donalisio@gmail.com>
2024-04-17 12:10:18 -0300

    Add footer to pdf

--- app/services/reports/images.rb
+++ app/services/reports/images.rb
@@ -16,7 +16,7 @@ class Reports::Images < Reports::Content
       table_width = @pdf.bounds.width
       @pdf.table([
         [{content: "FOTO #{n + 1}", background_color: PRIMARY_COLOR, font_style: :bold, align: :center}],
-        [{image: image.path, fit: [table_width, 270], position: :center, vposition: :center}]
+        [{image: image.path, fit: [table_width, 250], position: :center, vposition: :center}]
       ], width: table_width) do
         cells.border_color = PRIMARY_COLOR
       end
--- app/services/reports/pdf_generator.rb
+++ app/services/reports/pdf_generator.rb
@@ -4,20 +4,21 @@ class Reports::PdfGenerator < PdfGenerator
   private
 
   def generate_pdf
-    add_header_to_pdf
-    add_datetime_to_pdf
-    add_equipment_stats_to_pdf
-    add_room_stats_to_pdf
-    add_observations_to_pdf
-    add_images_to_pdf
+    add_header
+    add_datetime
+    add_equipment_stats
+    add_room_stats
+    add_observations
+    add_images
+    add_footer
     super
   end
 
-  def add_header_to_pdf
+  def add_header
     @pdf = Reports::Header.new(report, @pdf, client_logo).render
   end
 
-  def add_datetime_to_pdf
+  def add_datetime
     @pdf.move_down 10
     datetime = report&.created_at || Time.zone.now
     @pdf.table([
@@ -28,28 +29,28 @@ class Reports::PdfGenerator < PdfGenerator
     end
   end
 
-  def add_equipment_stats_to_pdf
+  def add_equipment_stats
     @pdf.move_down 10
     @pdf = Reports::EquipmentStats.new(report, @pdf).render
   end
 
-  def add_room_stats_to_pdf
+  def add_room_stats
     @pdf.move_down 10
     @pdf = Reports::RoomStats.new(report, @pdf).render
   end
 
-  def add_observations_to_pdf
+  def add_observations
     @pdf.move_down 10
     @pdf.table([
       [{content: "OBSERVACIONES", colspan: 3, background_color: PRIMARY_COLOR, font_style: :bold}],
-      [{content: report.observations, colspan: 3, height: 100, background_color: "f3f3f3"}]
+      [{content: report.observations, colspan: 3, height: 80, background_color: "f3f3f3"}]
     ], width: @pdf.bounds.width) do
       cells.borders = []
       cells.width = @pdf.bounds.width / 3
     end
   end
 
-  def add_images_to_pdf
+  def add_images
     return unless report.images.attached?
 
     new_page
@@ -58,8 +59,28 @@ class Reports::PdfGenerator < PdfGenerator
 
   def new_page
     @pdf.start_new_page
-    add_header_to_pdf
+    add_header
     @pdf.move_down 10
+  end
+
+  def add_footer
+    add_contact_info
+    add_page_numbers
+  end
+
+  def add_contact_info
+    @pdf.repeat(:all) do
+      @pdf.bounding_box([0, 25], width: @pdf.bounds.width, height: 50) do
+        @pdf.font "Helvetica", size: 10
+        @pdf.text "Tel: 3512 44 6662 | 3516 70 4660"
+        @pdf.text "Email: contacto@prela.com.ar"
+        @pdf.text "Dir: Hualfin 758 - Córdoba"
+      end
+    end
+  end
+
+  def add_page_numbers
+    @pdf.number_pages "Página <page> de <total>", at: [@pdf.bounds.right - 80, 25], size: 10
   end
 
   def report
