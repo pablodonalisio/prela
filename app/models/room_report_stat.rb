3605f58bb0a2b2d012dcf3586dcae5071dfc591a
Pablo Donalisio <pablo.donalisio@gmail.com>
2024-04-23 11:10:14 -0300

    Standarize reports inputs

--- app/models/room_report_stat.rb
+++ app/models/room_report_stat.rb
@@ -1,3 +1,9 @@
 class RoomReportStat < ApplicationRecord
+  ROOM_STATUS_OPTIONS = ["Correcto", "Incorrecto"]
+  AIR_CONDITIONING_OPTIONS = ["Correcto", "Incorrecto", "Falta refrigeración"]
   belongs_to :report
+
+  validates :room_status, :air_conditioning, presence: true
+  validates :room_status, inclusion: {in: ROOM_STATUS_OPTIONS, message: "Debe ser alguno de los siguientes: #{ROOM_STATUS_OPTIONS.join(", ")}"}
+  validates :air_conditioning, inclusion: {in: AIR_CONDITIONING_OPTIONS, message: "Debe ser alguno de los siguientes: #{AIR_CONDITIONING_OPTIONS.join(", ")}"}
 end
--- app/models/ups_report_stat.rb
+++ app/models/ups_report_stat.rb
@@ -1,5 +1,14 @@
 class UpsReportStat < ApplicationRecord
+  OPERATING_MODE_OPTIONS = ["Normal", "Bypass", "En Batería", "Fuera de servicio", "Apagado"]
+  PAT_STATE_OPTIONS = ["Correcto", "Incorrecto", "No tiene"]
+  VENTILATION_STATE_OPTIONS = ["Bien", "Mal", "No tiene"]
+  ALARMS_PRESENCE_OPTIONS = ["Si", "No"]
   belongs_to :report
 
-  validates :battery_charge, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}, allow_nil: true
+  validates :operating_mode, :associated_charge, :battery_charge, :voltage_input, :voltage_output, :pat_state, :ventilation_state, :alarms_presence, presence: true
+  validates :battery_charge, :associated_charge, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}, allow_nil: true
+  validates :operating_mode, inclusion: {in: OPERATING_MODE_OPTIONS, message: "Debe ser alguno de los siguientes: #{OPERATING_MODE_OPTIONS.join(", ")}"}
+  validates :pat_state, inclusion: {in: PAT_STATE_OPTIONS, message: "Debe ser alguno de los siguientes: #{PAT_STATE_OPTIONS.join(", ")}"}
+  validates :ventilation_state, inclusion: {in: VENTILATION_STATE_OPTIONS, message: "Debe ser alguno de los siguientes: #{VENTILATION_STATE_OPTIONS.join(", ")}"}
+  validates :alarms_presence, inclusion: {in: ALARMS_PRESENCE_OPTIONS, message: "Debe ser alguno de los siguientes: #{ALARMS_PRESENCE_OPTIONS.join(", ")}"}
 end
