231c57bd1a0b4b7967c734aae702a726f20817d6
Pablo Donalisio <pablo.donalisio@gmail.com>
2024-05-07 12:02:58 -0300

    fix: time zone validation for report date

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,5 +10,5 @@ class Report < ApplicationRecord
   accepts_nested_attributes_for :room_report_stat
 
   validates :date, presence: true
-  validates :date, comparison: {less_than_or_equal_to: Time.now, message: "No puede ser mayor a la fecha actual"}
+  validates :date, comparison: {less_than_or_equal_to: Time.zone.tomorrow, message: "No puede ser mayor a la fecha actual"}
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Report, type: :model do
     it "belongs to a location_equipment" do
       expect(report.location_equipment).to be_a(LocationEquipment)
     end
+
+    it "is invalid with a date greater than the current date" do
+      report.date = Time.zone.tomorrow + 1.day
+      expect(report).to_not be_valid
+    end
+
+    it "is valid with a date less than the current date" do
+      report.date = Time.zone.now
+      expect(report).to be_valid
+    end
   end
 end
