fb5e0079b9bcda42c98a728309a5b6dd1c772a54
Pablo Donalisio <pablo.donalisio@gmail.com>
2024-08-24 11:58:58 -0300

    feat: show not authorized flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,16 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def after_sign_out_path_for(user)
     new_user_session_path
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "No estas autorizado para realizar esta acción."
+    redirect_back_or_to(root_path)
   end
 end
