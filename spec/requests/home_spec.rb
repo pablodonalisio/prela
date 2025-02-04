8e58ffebc46ea24a513333904f65f054b1a63554
Pablo Donalisio <pablo.donalisio@gmail.com>
2025-02-04 18:21:56 -0300

    fix test

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Homes", type: :request do
   describe "GET /index" do
     it "returns http success" do
       get "/home/index"
-      expect(response).to have_http_status(:redirect)
+      expect(response).to have_http_status(:success)
     end
   end
 end
