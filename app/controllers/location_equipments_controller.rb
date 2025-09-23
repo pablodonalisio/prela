af884e3835a5cc297a56e34baf4747ebe7afc5f3
Pablo Donalisio <pablo.donalisio@gmail.com>
2025-09-23 14:44:14 -0300

    feat: add electrical panels to actives form

--- app/controllers/location_equipments_controller.rb
+++ app/controllers/location_equipments_controller.rb
@@ -64,7 +64,8 @@ class LocationEquipmentsController < ApplicationController
       .permit(:zone, :floor, :location_id, :equipment_id, :serial_number, :code, :form_link,
         :last_service, :next_service, :last_battery_change, :next_battery_change, :details, :status,
         :last_belt_change, :next_belt_change, :engine_serial_number, :power_unit_serial_number, :service_interval,
-        :battery_change_interval, :belt_change_interval)
+        :battery_change_interval, :belt_change_interval, :torque_interval, :last_torque, :next_torque,
+        :cleaning_interval, :last_cleaning, :next_cleaning)
   end
 
   def location_equipment
@@ -72,7 +73,7 @@ class LocationEquipmentsController < ApplicationController
   end
 
   def set_locations
-    @client = @location_equipment.location&.client
+    @client = @location_equipment.location&.client || Client.find_by(id: params[:location_equipment][:client_id])
     @locations = Location.where(client_id: @client&.id)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_173822) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_18_184238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -126,6 +126,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_173822) do
     t.integer "service_interval", default: 1
     t.integer "battery_change_interval", default: 2
     t.integer "belt_change_interval", default: 5
+    t.integer "torque_interval", default: 1
+    t.date "last_torque"
+    t.date "next_torque"
+    t.integer "cleaning_interval", default: 1
+    t.date "last_cleaning"
+    t.date "next_cleaning"
     t.index ["equipment_id"], name: "index_location_equipments_on_equipment_id"
     t.index ["location_id"], name: "index_location_equipments_on_location_id"
   end
