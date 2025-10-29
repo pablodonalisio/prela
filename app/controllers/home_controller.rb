770509ed69f3e767e6a1acfba08f582a95093f1b
Pablo Donalisio <pablo.donalisio@gmail.com>
2025-10-29 17:13:31 -0300

    feat(ControlPanel): add filters by client and kind

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
   def index
     authorize :home, :index?
     @links = Link.all if current_user.admin?
-    @overdue_services = policy_scope(ServiceDate).overdue_next_service_dates_by_equipment_kind
+    @overdue_services = policy_scope(ServiceDate.filter(service_filter_params)).overdue_next_service_dates_by_equipment_kind
+  end
+
+  private
+
+  def service_filter_params
+    params.slice(:kind, :client_id)
   end
 end
--- app/models/service_date.rb
+++ app/models/service_date.rb
@@ -1,10 +1,17 @@
 class ServiceDate < ApplicationRecord
+  include Filterable
+
   belongs_to :location_equipment
   belongs_to :activity, optional: true
 
   enum :kind, {service: 0, battery_change: 1, belt_change: 2, torque: 3, cleaning: 4, srt_900: 5, thermography: 6, electrical_approval: 7}
 
   validates :kind, :date, presence: true
+
+  scope :by_kind, ->(kind) { where(kind:) }
+  scope :by_client_id, ->(client_id) {
+    joins(location_equipment: {location: :client}).where(clients: {id: client_id})
+  }
 
   class << self
     def next_service_dates(selected_columns = "*")
