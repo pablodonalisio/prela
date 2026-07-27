fb1693846932c255c54fdb04b081023f4a76c933
Pablo Donalisio <pablo.donalisio@gmail.com>
2026-07-27 12:15:10 -0300

    feat: add paginator to LocationEquipment and Equipment index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include Pagy::Frontend
+
   FLASH_CLASSES = {
     notice: "alert alert-info",
     success: "alert alert-success",
---
+++ config/initializers/pagy.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require "pagy/extras/bootstrap"
+
+Pagy::DEFAULT[:limit] = 12
+Pagy::DEFAULT.freeze
